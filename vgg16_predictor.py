c86e9e1e218b12f121b320eb319e3bfb663280cc
Daniel Varga <daniel.varga@prezi.com>
2016-08-10 14:25:35 +0200

    predictor outputs text labels, top5

--- vgg16_predictor.py
+++ vgg16_predictor.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
@@ -56,16 +57,26 @@ def VGG_16(weights_path=None):
     return model
 
 if __name__ == "__main__":
-    im = scipy.misc.imresize(scipy.misc.imread(sys.argv[1]), (224, 224)).astype(np.float32)
-    im[:,:,0] -= 103.939
-    im[:,:,1] -= 116.779
-    im[:,:,2] -= 123.68
-    im = im.transpose((2,0,1))
-    im = np.expand_dims(im, axis=0)
-
     # Test pretrained model
     model = VGG_16('vgg16_weights.h5')
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
-    out = model.predict(im)
-    print np.argmax(out)
+
+    class_names = json.load(open("imagenet_class_index.json"))
+
+    for filename in sys.argv[1:]:
+        im = scipy.misc.imresize(scipy.misc.imread(filename), (224, 224)).astype(np.float32)
+        im = im[:, :, :3]
+        im[:,:,0] -= 103.939
+        im[:,:,1] -= 116.779
+        im[:,:,2] -= 123.68
+        im = im.transpose((2,0,1))
+        im = np.expand_dims(im, axis=0)
+
+        out = model.predict(im).flatten()
+        prediction = np.argmax(out)
+        top5 = sorted(zip(out, range(len(out))), reverse=True)[:5] # (probability, class_id) pairs.
+        top5_probs_and_names = ["%s = %f ," % (class_names[str(prediction)][1], probability) for (probability, prediction) in top5]
+        top5_names = [class_names[str(prediction)][1] for (probability, prediction) in top5]
+
+        print " ".join(top5_probs_and_names).encode("utf-8")
